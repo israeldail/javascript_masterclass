7f8649c021f62c7cd49b0cc9645ee2d4ccf2245b
israeldail <israeldail2@gmail.com>
2024-04-28 12:58:40 -0400

    quiz

---
+++ ninjaquiz/quiz.js
@@ -0,0 +1,8 @@
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+
+console.log("logged")
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+});
